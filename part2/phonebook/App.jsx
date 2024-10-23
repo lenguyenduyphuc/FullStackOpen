b7841333032c347050eee2d5594c7129709c9051
Phuc <duyphuclenguyen@gmail.com>
2024-10-22 23:36:03 -0400

    updated

---
+++ part2/phonebook/App.jsx
@@ -0,0 +1,95 @@
+import { useState} from 'react'
+
+const App = () => {
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    ])
+    
+  const [newName, setNewName] = useState('')
+  const [newNum, setNewNum] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+
+
+
+  const addName = (event) => {
+    event.preventDefault()
+    //check if the person has already in the phonebook
+    console.log(persons)
+    if (persons.some(person => person.name === newName)){
+        alert(`${newName} is already in the phonebook`)
+    } else {
+        //Add the new person to the phonebook
+        const newPerson = {
+            name: newName,
+            number: newNum,
+            id: persons.length + 1
+        }
+        console.log(newName)
+        setPersons(persons.concat(newPerson))//Add the person to the state
+        setNewNum('')
+        setNewName('')
+        }
+    }
+
+
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value)
+    console.log(event.target.value)
+  }
+
+  const handleNewNum = (event) => {
+    setNewNum(event.target.value)
+  }
+
+  const handleFilterChange = (event) =>{
+    setNewFilter(event.target.value)
+  }
+
+  //check if filter is empty. If not empty mean that user has type in filter the array
+  const personsToShow = newFilter
+    ? persons.filter(person => person.name.includes(newFilter))
+    : persons;
+
+  return (
+    <div>
+        <h2>Phonebook</h2>
+        <div>
+            filter: <input
+                type= 'text'
+                value={newFilter}
+                onChange={handleFilterChange}
+            />
+        </div>
+      <h2>add a new</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input 
+                type='text'
+                value={newName}
+                onChange={handleNewName}
+          />
+        </div>
+        <div>
+            number: <input
+                type='text'
+                value={newNum}
+                onChange={handleNewNum}
+            />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        {personsToShow.map((person) => (
+            <p key={person.id}>{person.name} {person.number}</p>
+        ))}
+    </div>
+  )
+}
+
+export default App
