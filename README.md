69078fb662f221ff92596f4828e6fb1fa27f8f89
Phuc <duyphuclenguyen@gmail.com>
2024-10-24 16:01:19 -0400

    updated the files of css and create more vite files

--- README.md
+++
@@ -1,3 +0,0 @@
-<h2>FullStack Open</h2>
-
-In the progress of becoming better
--- part2/courseinfo+/src/App.jsx
+++ part2/courseinfo+/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Course from './components/Course'
+import './App.css'
 
 const App = () => {
   const courses = [
