1b1792c9996f0f82ffbbcae9b1019e4a420a7fd3
Phuc <duyphuclenguyen@gmail.com>
2024-10-28 15:10:22 -0400

    updated

--- part3/testing/index.js
+++ part3/testing/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+const cors = require('cors')
 let notes = [
   {
     id: 1,
@@ -28,8 +28,11 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
+
+
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({error: 'Unknown endpoint'})
