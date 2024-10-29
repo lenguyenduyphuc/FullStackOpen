d533f5db10cb6b90d87bb847377fcdcc1b97d8d1
Phuc <duyphuclenguyen@gmail.com>
2024-10-29 15:08:50 -0400

    part 3b

--- part3/testing/index.js
+++ part3/testing/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+
 let notes = [
   {
     id: 1,
@@ -31,7 +32,7 @@ const requestLogger = (request, response, next) => {
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
-
+app.use(express.static('dist'))
 
 
 const unknownEndpoint = (request, response) => {
