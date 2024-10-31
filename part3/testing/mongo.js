a944df8f04e6504bec2eeb9dcb904bf4a868e45e
Phuc <duyphuclenguyen@gmail.com>
2024-10-31 13:56:58 -0400

    database

--- part3/testing/mongo.js
+++ part3/testing/mongo.js
@@ -1,32 +1,38 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length < 3){
-    console.log('give password as argument')
-    process.exit(1)
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://duyphuclenguyen:<db_password>@cluster0.pezqi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-
-mongoose.set(`strictQuery`, false)
+const url =
+ 'mongodb+srv://phuc:phuc2208@cluster0.pezqi.mongodb.net/myDatabase?retryWrites=true&w=majority'
+mongoose.set('strictQuery',false)
 
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    imnportant: Boolean,
+  content: String,
+  important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note ({
-    content: 'HTML is easy',
-    important: true,
-})
+// const note = new Note({
+//   content: 'HTML is easy',
+//   important: false
+// })
 
-note.save().then(result => {
-    console.log(`note saved!`)
+Note.find({important: true}).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
     mongoose.connection.close()
 })
 
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
