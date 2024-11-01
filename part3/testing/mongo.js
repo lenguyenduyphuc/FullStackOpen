f3f5714972b15fb91177ccc12a24bf5b9a46377d
Phuc <duyphuclenguyen@gmail.com>
2024-11-01 11:18:50 -0400

    updated note to part3-4

--- part3/testing/mongo.js
+++ part3/testing/mongo.js
@@ -9,38 +9,31 @@ const password = process.argv[2]
 
 const url =
  `mongodb+srv://phuc:${password}@cluster0.pezqi.mongodb.net/myDatabase?retryWrites=true&w=majority`
-mongoose.set('strictQuery',false)
 
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
-const note = new Note({
-  content: 'HTML is easy',
-  important: true
-})
-
-Note.find({}).then(result => {
+mongoose.set('strictQuery', false)
+mongoose.connect(url).then(() => {
+  const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+  })
+  
+  const Note = mongoose.model('Note', noteSchema)
+  
+  const note = new Note({
+    content: 'HTML is x',
+    important: true,
+  })
+  
+  /*
+  note.save().then(result => {
+    console.log('note saved!')
+    mongoose.connection.close()
+  })
+  */
+  Note.find({}).then(result => {
     result.forEach(note => {
-        console.log(note)
+      console.log(note)
     })
     mongoose.connection.close()
+  })
 })
-
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
-// })
