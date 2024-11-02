c92b1f6690fe7bb2989f2e23d463c690a36c2aeb
Phuc <duyphuclenguyen@gmail.com>
2024-11-02 12:20:46 -0400

    updated person and phone number

--- part3/phonebookbackend/models/person.js
+++ part3/phonebookbackend/models/person.js
@@ -4,15 +4,15 @@ const mongoose = require('mongoose')
 mongoose.set(`strictQuery`, false)
 
 const url = process.env.MONGODB_URI
-console.log(`connecting to`, url)
+console.log('connecting to', url)
 
 mongoose.connect(url)
-.then(() => {
+  .then(() => {
     console.log('connected to MongoDB')
-})
-.catch(error => {
-    console.log('error connecting to MongoDB', error.message)
-})
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
     name: {
