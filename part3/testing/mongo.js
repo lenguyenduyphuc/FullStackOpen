4446af0b3a3f49262a6570b8d465435d10faed7f
Phuc <duyphuclenguyen@gmail.com>
2024-11-02 20:24:24 -0400

    Finished part3

--- part3/testing/mongo.js
+++ part3/testing/mongo.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 
-const uri = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(error => console.error('Error connecting to MongoDB:', error.message))
 
@@ -19,10 +19,10 @@ mongoose.connect(url).then(() => {
 
   const Note = mongoose.model('Note', noteSchema)
 
-  const note = new Note({
-    content: 'HTML is x',
-    important: true,
-  })
+  //   const note = new Note({
+  //     content: 'HTML is x',
+  //     important: true,
+  //   })
 
   /*
   note.save().then(result => {
