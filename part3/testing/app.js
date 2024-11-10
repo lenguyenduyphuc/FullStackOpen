a781a4953c42f182224575b7e1ff3849e7a31c24
Phuc <duyphuclenguyen@gmail.com>
2024-11-10 15:31:15 -0500

    Fixed

--- part3/testing/app.js
+++ part3/testing/app.js
@@ -7,17 +7,17 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-mongoose.set(`strictQuery`, false)
+mongoose.set('strictQuery', false)
 
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI)
-    .then(() => {
-        logger.info('connected to MONGODB')
-    })
-    .catch(error => {
-        logger.error('error connectiong to MongoDB:', error.message)
-    })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.static('dist'))
--- part3/testing/mongo.js
+++ part3/testing/mongo.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
-require('dotenv').config()
 
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
 
-const url = process.env.MONGODB_URI
+const password = process.argv[2]
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(error => console.error('Error connecting to MongoDB:', error.message))
-
-
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/noteApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url).then(() => {
@@ -19,12 +19,12 @@ mongoose.connect(url).then(() => {
 
   const Note = mongoose.model('Note', noteSchema)
 
-  //   const note = new Note({
-  //     content: 'HTML is x',
-  //     important: true,
-  //   })
-
   /*
+  const note = new Note({
+    content: 'HTML is x',
+    important: true,
+  })
+
   note.save().then(result => {
     console.log('note saved!')
     mongoose.connection.close()
