116972af3b6749cd31ce09633d1baa3c66248677
Phuc <duyphuclenguyen@gmail.com>
2024-11-11 16:00:58 -0500

    test totalLike

--- part4/bloglist/backend/utils/list_helper.js
+++ part4/bloglist/backend/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const sum =  ((total, blog) => {
+        return total + blog.likes
+    })
+
+    return blogs.length === 0 
+    ? 0
+    : total = blogs.reduce(sum, 0)
+}
+
 module.exports =  {
-    dummy
+    dummy,
+    totalLikes
 }
