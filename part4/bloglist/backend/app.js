8a0daea308d71e4734f9fc079cd291433f2301f8
Phuc <duyphuclenguyen@gmail.com>
2024-11-11 12:02:06 -0500

    Changed the app.js

--- part4/bloglist/backend/app.js
+++ part4/bloglist/backend/app.js
@@ -2,38 +2,15 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const blogsRouter = require('./controllers/blogs')
 
 const Blog = require('./models/blog')
 
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
-
-app.delete('/api/blogs/:id', (request, response, next) => {
-    Blog.findByIdAndDelete(request.params.id)
-        .then(() => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
-})
 
 
 module.exports = app
