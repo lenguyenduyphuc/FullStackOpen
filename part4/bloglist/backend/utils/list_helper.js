ed60c279a7cc64db0b632f6d42c6991078b8fbca
Phuc <duyphuclenguyen@gmail.com>
2024-11-11 16:26:45 -0500

    make tests for favorite Blogs

--- part4/bloglist/backend/utils/list_helper.js
+++ part4/bloglist/backend/utils/list_helper.js
@@ -12,7 +12,33 @@ const totalLikes = (blogs) => {
     : total = blogs.reduce(sum, 0)
 }
 
+const favoriteBLog = (blogs) => {
+    if (blogs.length === 0){
+        return null
+    }
+    
+    let max = 0
+    let favorite = null
+    blogs.forEach(blog => {
+        if (blog.likes > max){
+            max = blog.likes,
+            favorite = blog
+        }
+    })
+
+    let blog = {
+        title: favorite.title, 
+        author: favorite.author,
+        likes: favorite.likes
+    }
+    
+    console.log(blog)
+
+    return blog
+}
+
 module.exports =  {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBLog
 }
