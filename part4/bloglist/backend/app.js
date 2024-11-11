950ade75e5e864577f8370e9c65eab66664ed681
Phuc <duyphuclenguyen@gmail.com>
2024-11-10 20:25:52 -0500

    Refractor index.js

---
+++ part4/bloglist/backend/app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+
+const Blog = require('./models/blog')
+
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
+})
+
+app.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body)
+
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+})
+
+app.delete('/api/blogs/:id', (request, response, next) => {
+    Blog.findByIdAndDelete(request.params.id)
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+
+module.exports = app
--- part4/bloglist/backend/index.js
+++ part4/bloglist/backend/index.js
@@ -1,41 +1,7 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const app = require('./app') //Actual express application
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const Blog = require('./models/blog')
-
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
-
-app.delete('/api/blogs/:id', (request, response, next) => {
-    Blog.findByIdAndDelete(request.params.id)
-        .then(() => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+    logger.info(`Server is running on port ${config.PORT}`)
 })
