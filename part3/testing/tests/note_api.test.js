ab20bc9231067ec2c7c0e434708275349397be81
Phuc <duyphuclenguyen@gmail.com>
2024-11-12 15:12:32 -0500

    Add test when add new note

--- part3/testing/tests/note_api.test.js
+++ part3/testing/tests/note_api.test.js
@@ -1,11 +1,11 @@
 const { test, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
-const Note = require('../models/note')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+
+const Note = require('../models/note')
 
 const initialNotes = [
   {
@@ -20,8 +20,10 @@ const initialNotes = [
 
 beforeEach(async () => {
   await Note.deleteMany({})
+
   let noteObject = new Note(initialNotes[0])
   await noteObject.save()
+
   noteObject = new Note(initialNotes[1])
   await noteObject.save()
 })
@@ -44,6 +46,27 @@ test('the first note is about HTTP methods', async () => {
 
   const contents = response.body.map(e => e.content)
   assert(contents.includes('HTML is easy'))
+})
+
+test('a valid note can be added', async () => {
+  const newNote = {
+    content: 'async/await simplifies making async calls',
+    important: true,
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(201)
+    .expect('Content-type', /application\/json/)
+
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(r => r.content)
+
+  assert.strictEqual(response.body.length, initialNotes.length + 1)
+
+  assert(contents.includes('async/await simplifies making async calls'))
 })
 
 after(async () => {
