16a64b48e768222658157c9241e2580f387254b7
Phuc <duyphuclenguyen@gmail.com>
2024-11-13 15:03:08 -0500

    Add express-async-errors library and remove try and catch

--- part3/testing/controllers/notes.js
+++ part3/testing/controllers/notes.js
@@ -27,34 +27,22 @@ notesRouter.post('/', async (request, response, next) => {
     important: body.important || false,
   })
 
-  try {
-    const savedNote = await note.save()
-    response.status(201).json(savedNote)
-  }
-  catch (exception) {
-    next(exception)
-  }
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
+  
 })
 
 notesRouter.get('/:id', async (request, response, next) => {
-	try {
 		const note = await Note.findById(request.params.id)
 		if (note) {
 			response.json(note)
 		} else {
 			response.status(204).end()
 		}
-	} catch (exception) {
-		next(exception)
-	}
 })
 
 notesRouter.delete('/:id', async () => {
-	try {
 		await Note.findByIdAndDelete(request.params.id)
 		response.status(204).end()
-	} catch (exception) {
-		next(exception)
-	}
 })
 module.exports = notesRouter
