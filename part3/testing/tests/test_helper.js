96654aae560d469192592d69a4827d12dd186ec7
Phuc <duyphuclenguyen@gmail.com>
2024-11-14 13:53:18 -0500

    Refractor tests

--- part3/testing/tests/test_helper.js
+++ part3/testing/tests/test_helper.js
@@ -1,34 +1,29 @@
 const Note = require('../models/note')
 
-
-//database notes
 const initialNotes = [
-    {
-      content: 'HTML is easy',
-      important: false
-    },
-    {
-      content: 'Browser can execute only JavaScript',
-      important: true
-    }
+  {
+    content: 'HTML is easy',
+    important: false
+  },
+  {
+    content: 'Browser can execute only JavaScript',
+    important: true
+  }
 ]
 
-//create database object
-const nonExistId = async () => {
-    const note = new Note({ content: 'willremovethissoon'})
-    await note.save()
-    await note.deleteOne()
+const nonExistingId = async () => {
+  const note = new Note({ content: 'willremovethissoon' })
+  await note.save()
+  await note.deleteOne()
 
-    return  note._id.toString()
+  return note._id.toString()
 }
 
-//check note store in dababase
 const notesInDb = async () => {
-    const notes = await Note.find({})
-
-    return notes.map(note => note.toJSON())
+  const notes = await Note.find({})
+  return notes.map(note => note.toJSON())
 }
 
 module.exports = {
-    initialNotes, nonExistId, notesInDb
+  initialNotes, nonExistingId, notesInDb
 }
--- part3/testing/utils/for_testing.js
+++ part3/testing/utils/for_testing.js
@@ -1,21 +1,21 @@
 const reverse = (string) => {
     return string
-    .split('')
-    .reverse()
-    .join('')
-}
-
-const average = (array) => {
+      .split('')
+      .reverse()
+      .join('')
+  }
+  
+  const average = (array) => {
     const reducer = (sum, item) => {
-        return sum + item
+      return sum + item
     }
-
+  
     return array.length === 0
-    ? 0
-    : array.reduce(reducer, 0) / array.length
-}
-
-module.exports = {
+      ? 0
+      : array.reduce(reducer, 0) / array.length
+  }
+  
+  module.exports = {
     reverse,
-    average
-}
+    average,
+  }
