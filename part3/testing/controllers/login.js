0d94dcd6c3fc1696545b9b630ced607602e2120a
Phuc <duyphuclenguyen@gmail.com>
2024-11-15 11:35:50 -0500

    Add log in users

--- part3/testing/controllers/login.js
+++ part3/testing/controllers/login.js
@@ -2,32 +2,33 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const loginRouter = require('express').Router()
 const User = require('../models/user')
+require('dotenv').config();
+
 
 loginRouter.post('/', async (request, response) => {
-    const { username, password } = request.body
-    
-    const user = await User.findOne({ username })
+  const { username, password } = request.body
 
-    const passwordCorrect = user === null
-        ? false
-        : await bcrypt.compare(password, user.passwordHash)
+  const user = await User.findOne({ username })
+  const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.passwordHash)
 
-    if (!(user && passwordCorrect)){
-        return response.status(401).json({
-            error: 'Invalid username or password'
-        })
-    }
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: 'invalid username or password'
+    })
+  }
 
-    const userForToken = {
-        username: user.username,
-        id: user_id,
-    }
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  }
 
-    const token = jwt.sign(userForToken, process.env.SECRET)
+  const token = jwt.sign(userForToken, process.env.SECRET)
 
-    response
-        .status(200)
-        .send({ token, username: user.username, name: user.name})
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name })
 })
 
 module.exports = loginRouter
