fb5b899a7864da06d73a67444c5a31d47a321109
Phuc <duyphuclenguyen@gmail.com>
2024-11-15 20:20:08 -0500

    Create new users and make changes to blog tests

--- part4/bloglist/backend/testing/blog_api.test.js
+++ part4/bloglist/backend/testing/blog_api.test.js
@@ -1,128 +1,195 @@
-const { test, after, expect, beforeEach} = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
+const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const helper = require('./test_helper')
 const api = supertest(app)
 
+
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 
-
-beforeEach(async () => {
-    //Create blogs without user
-    await Blog.deleteMany({})
-    await Blog.insertMany(helper.initialBlogs)
-})
-
-test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-})
-
-test('the unique identifier property of the blog posts is named id', async () => {
-    const blogsAtStart = await helper.blogsInDb()
-
-    const blogToView = blogsAtStart[0]
-
-    const resultBlog = await api
-        .get(`/api/blogs/${blogToView.id}`)
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-
-    assert.deepStrictEqual(resultBlog.body, blogToView)
-})
-
-test('Successfully create a new blog post', async () => {
-    const newBlog = {
-        _id: "5a422bc61b54a676234d17fc",
-        title: "Type wars",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-        likes: 2,
-        __v: 0
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-
-    const blogsAtEnd = await helper.blogsInDb()
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
-})
-
-test('Check the like property of the blogs', async () => {
-    const newBlog = {
-        _id: "5a422bc61b54a676234d17fc",
-        title: "Breh wars",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-
-    const blogsAtEnd = await helper.blogsInDb()
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
-
-    const blogs = blogsAtEnd.find(r => r.title === "Breh wars")
-    assert.deepStrictEqual(blogs.likes, 0)
-})
-
-test('Check the title or url properties', async () => {
-    const newBlog = {
-        _id: "035313233633",
-        title: "Testing title and url",
-        likes: 5
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
-        .expect('Content-Type', /application\/json/)
-})
-
-test('Delete a single blog', async () => {
-    const blogsAtStart = await helper.blogsInDb()
-    const blogToDelete = blogsAtStart[0]
-
-    await api
-        .delete(`/api/blogs/${blogToDelete.id}`)
-        .expect(204)
+describe('when there is initially some blogs saved', () => {
+    beforeEach(async () => {
+        //Create blogs without user
+        await Blog.deleteMany({})
+        await Blog.insertMany(helper.initialBlogs)
+    })
     
-    const blogsAtEnd = await helper.blogsInDb()
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
-
-    const contents = blogsAtEnd.map(r => r.id)
-    assert(!contents.includes(blogToDelete.id))
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+    
+    test('the unique identifier property of the blog posts is named id', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+    
+        const blogToView = blogsAtStart[0]
+    
+        const resultBlog = await api
+            .get(`/api/blogs/${blogToView.id}`)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    
+        assert.deepStrictEqual(resultBlog.body, blogToView)
+    })
 })
 
-test('Updated a new note', async () => {
 
-    const updatedBlog = {
-        _id: "5a422ba71b54a676234d17fb",
-        title: "It is a good thing",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-        likes: 0,
-        __v: 0
-    }
+describe('Adding new blogs', () => {
+    test('Successfully create a new blog post', async () => {
+        const newBlog = {
+            _id: "5a422bc61b54a676234d17fc",
+            title: "Type wars",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+            likes: 2,
+            __v: 0
+        }
+    
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+    
+        const blogsAtEnd = await helper.blogsInDb()
+        assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+    })
+    
+    test('Check the like property of the blogs', async () => {
+        const blogsAtStart = await helper.blogsInDb()
 
-    await api
-        .put(`/api/blogs/${updatedBlog._id}`)
-        .send(updatedBlog)
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
+        const newBlog = {
+            _id: "5a422bc61b54a676234d17fc",
+            title: "Breh wars",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+        }
+    
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+    
+        const blogsAtEnd = await helper.blogsInDb()
+        assert.strictEqual(blogsAtEnd.length, blogsAtStart.length + 1)
+    
+        const blogs = blogsAtEnd.find(r => r.title === newBlog.title)
+        assert.deepStrictEqual(blogs.likes, 0)
+    })
+    
+    test('Check the title or url properties', async () => {
+        const newBlog = {
+            _id: "035313233633",
+            title: "Testing title and url",
+            likes: 5
+        }
+    
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+    })
+})
 
+
+describe('Deletion of blogs and updated blogs', () => {
+    test('Delete a single blog', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const blogToDelete = blogsAtStart[0]
+    
+        await api
+            .delete(`/api/blogs/${blogToDelete.id}`)
+            .expect(204)
+        
+        const blogsAtEnd = await helper.blogsInDb()
+        assert.strictEqual(blogsAtEnd.length, blogsAtStart.length - 1)
+    
+        const contents = blogsAtEnd.map(r => r.id)
+        assert(!contents.includes(blogToDelete.id))
+    })
+    
+    test('Updated a new note', async () => {
+    
+        const updatedBlog = {
+            _id: "5a422ba71b54a676234d17fb",
+            title: "It is a good thing",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+            likes: 0,
+            __v: 0
+        }
+    
+        await api
+            .put(`/api/blogs/${updatedBlog._id}`)
+            .send(updatedBlog)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    
+    })
+})
+
+
+describe('when there is initially one user in db', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+
+        const passwordHash = await bcrypt.hash('sekret', 10)
+        const user = new User({ username: 'root', passwordHash})
+
+        await user.save()
+    })
+    
+    test('creation succeeds with a fresh username', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'mluukkai',
+            name: 'Matti Luukkainen',
+            password: 'salainen',
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+        
+        const usersAtEnd = await helper.usersInDb()
+        assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+        const usernames = usersAtEnd.map(user => user.username)
+        assert(usernames.includes(newUser.username))
+    })
+
+    test('creation fails with proper statuscode and message if username already taken', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'root',
+            name: 'Superuser',
+            password: 'salainen',
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAtEnd = await helper.usersInDb()
+        assert(result.body.error.includes('expected `username` to be unique'))
+
+        assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+    })
 })
 
 after(async () => {
--- part4/bloglist/backend/utils/middleware.js
+++ part4/bloglist/backend/utils/middleware.js
@@ -12,17 +12,17 @@ const unknownEndpoint = (request, response) => {
     response.status(400).send({error: "unknownEndpoint"})
 }
 
-const errorHandler = (error, request, response) => {
-    logger.error(error.message)
-
-    if (error.name === "CastError"){
-        return response.status(400).send({error: "Cast Error"})
-    } else if (error.name === "ValidationError"){
-        return response.status(400).json({error: error.message})
+const errorHandler = (error, request, response, next) => {
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
+    } else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
+      return response.status(400).json({ error: 'expected `username` to be unique' })
     }
-
+  
     next(error)
-}
+  }
 
 module.exports = {
     requestLogger,
