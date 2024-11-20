3821b5fc88e5090c63fb50eecc3cc4c6139f4334
Phuc <duyphuclenguyen@gmail.com>
2024-11-20 14:18:52 -0500

    Add test for failed login

--- part2/my-testing/playwright.config.cjs
+++ part2/my-testing/playwright.config.cjs
@@ -12,23 +12,14 @@ import { defineConfig, devices } from '@playwright/test'
  */
 module.exports = defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
+  timeout: 3000,
   fullyParallel: false,
   workers: 1,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    //baseURL: 'http://localhost:5173',
     trace: 'on-first-retry',
   },
 
@@ -48,26 +39,6 @@ module.exports = defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
   /* Run your local dev server before starting the tests */
@@ -77,4 +48,3 @@ module.exports = defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
--- part2/my-testing/tests/note_app.spec.js
+++ part2/my-testing/tests/note_app.spec.js
@@ -3,7 +3,7 @@ import { beforeEach, describe } from 'node:test'
 
 describe('Note app', () => {
   beforeEach(async ({ page, request }) => {
-    await request.post('http://localhost:3001/api/testing/reset')
+    await request.post('http:localhost:3001/api/testing/reset')
     await request.post('http://localhost:3001/api/users', {
       data: {
         name: 'Matti Luukkainen',
@@ -21,11 +21,12 @@ describe('Note app', () => {
     await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2024')).toBeVisible()
   })
 
-  test('user can log in', async ({ page }) => {
+  test('login form can be opened', async ({ page }) => {
     await page.getByRole('button', { name: 'log in' }).click()
     await page.getByTestId('username').fill('mluukkai')
     await page.getByTestId('password').fill('salainen')
     await page.getByRole('button', { name: 'login' }).click()
+  
     await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
   })
 
@@ -41,20 +42,34 @@ describe('Note app', () => {
       await page.getByRole('button', { name: 'new note' }).click()
       await page.getByRole('textbox').fill('a note created by playwright')
       await page.getByRole('button', { name: 'save' }).click()
-      await expect(page.getByText('a note created by playwright')).toBeVisible()
-    })
-  })
-  
-  describe('and a note exists', () => {
-    beforeEach(async ({ page }) => {
-      await page.getByRole('button', { name: 'new note' }).click()
-      await page.getByRole('textbox').fill('another note by playwright')
-      await page.getByRole('button', { name: 'save' }).click()
+      await expect(await page.getByText('a note created by playwright')).toBeVisible()
     })
 
-    test('importance can be changed', async ({ page }) => {
-      await page.getByRole('button', { name: 'make not important' }).click()
-      await expect(page.getByText('make important')).toBeVisible()
+    describe('and a note exists', () => {
+      beforeEach(async ({ page }) => {
+        await page.getByRole('button', { name: 'new note' }).click()
+        await page.getByRole('textbox').fill('another note by playwright')
+        await page.getByRole('button', { name: 'save' }).click()
+      })
+  
+      test('importance can be changed', async ({ page }) => {
+        await page.getByRole('button', { name: 'make not important' }).click()
+        await expect(await page.getByText('make important')).toBeVisible()
+      })
     })
-  })
+
+    test('login fails with wrong password', async ({ page }) =>{
+      await page.getByRole('button', { name: 'log in' }).click()
+      await page.getByTestId('username').fill('mluukkai')
+      await page.getByTestId('password').fill('wrong')
+      await page.getByRole('button', { name: 'login' }).click()
+    
+      const errorDiv = await page.locator('.error')
+      await expect(errorDiv).toContainText('wrong credentials')
+      await expect(errorDiv).toHaveCSS('border-style', 'solid')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+    
+      await expect(page.getByText('Matti Luukkainen logged in')).not.toBeVisible()
+    })
+  })  
 })
