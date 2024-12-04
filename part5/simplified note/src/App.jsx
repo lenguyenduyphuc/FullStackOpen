5c540dd9139db8b03ee41fb089fe4f197c2a7ac0
NguyenDuyPhucLe <duyphuclenguyen@gmail.com>
2024-12-04 12:22:34 -0500

    Add notes filter function

--- part5/simplified note/src/App.jsx
+++ part5/simplified note/src/App.jsx
@@ -1,5 +1,6 @@
 import NewNote from "./components/NewNote"
 import Notes from "./components/Notes"
+import VisibilityFilter from "./components/VisibilityFilter"
 
 const App = () => {
   const filterSelected = (value) => {
@@ -9,14 +10,7 @@ const App = () => {
   return (
     <div>
       <NewNote/>
-      <div>
-        all <input type='radio' name='filter'
-          onChange={() => filterSelected('ALL')} />
-        important    <input type="radio" name="filter"
-          onChange={() => filterSelected('IMPORTANT')} />
-        nonimportant <input type="radio" name="filter"
-          onChange={() => filterSelected('NONIMPORTANT')} />
-      </div>
+      <VisibilityFilter/>
       <Notes/>
     </div>
   )
--- part5/simplified note/src/main.jsx
+++ part5/simplified note/src/main.jsx
@@ -20,14 +20,14 @@ console.log(store.getState())
 store.subscribe(() => console.log(store.getState()))
 store.dispatch(filterChange('IMPORTANT'))
 store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>
-// )
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-      <div />
-    </Provider>
-  )
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <Provider store={store}>
+//       <div />
+//     </Provider>
+//   )
