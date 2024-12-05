1928cedec4973750c26977163452d248d3c284ce
NguyenDuyPhucLe <duyphuclenguyen@gmail.com>
2024-12-04 19:57:23 -0500

    Implement reduxjs toolkit

--- part5/simplified note/src/main.jsx
+++ part5/simplified note/src/main.jsx
@@ -1,19 +1,20 @@
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
+import { combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import { createNote } from './reducers/noteReducer'
 import { filterChange } from './reducers/filterReducer'
+import { configureStore } from '@reduxjs/toolkit'
 import App from './App'
 
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 
-const reducer = combineReducers({
+const store = configureStore({
+  reducer: {
     notes: noteReducer,
     filter: filterReducer
+  }
 })
-
-const store = createStore(reducer)
 
 console.log(store.getState())
 
