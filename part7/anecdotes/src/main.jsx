6c30f37a529802627a9c3ae31354a81c5945ba2a
NguyenDuyPhucLe <duyphuclenguyen@gmail.com>
2024-12-08 17:34:10 -0500

    Add React Router to the application so that by clicking links in the Menu component the view can be changed

---
+++ part7/anecdotes/src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import { BrowserRouter as Router } from 'react-router'
+createRoot(document.getElementById('root')).render(
+  <Router>
+    <App />
+  </Router>,
+)
