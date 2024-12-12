6d54ce7860d309b4222300c8803098b365e83a8a
NguyenDuyPhucLe <duyphuclenguyen@gmail.com>
2024-12-12 12:39:21 -0500

    Webpack-dev-server

--- part7/bundles/src/index.js
+++ part7/bundles/src/index.js
@@ -1,7 +1,7 @@
+import 'core-js/stable/index.js'
+import 'regenerator-runtime/runtime.js'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 
-const hello =  name => {
-  console.log(`hello ${name}`)
-}
-
-App()
+ReactDOM.createRoot(document.getElementById('root')).render(< App />)
--- part7/bundles/webpack.config.js
+++ part7/bundles/webpack.config.js
@@ -5,7 +5,27 @@ const config = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'main.js'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'build'),
+    compress: true,
+    port: 3000,
+  },
 }
 
 module.exports = config
