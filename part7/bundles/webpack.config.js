7debe1233388420d8805e8bd88cc49929f648b4e
NguyenDuyPhucLe <duyphuclenguyen@gmail.com>
2024-12-14 21:09:08 -0500

    Updated

--- part7/bundles/webpack.config.js
+++ part7/bundles/webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path')
 const webpack = require('webpack')
+
 const config = (env, argv) => {
   console.log('argv', argv.mode)
 
   const backend_url = argv.mode === 'production'
   ? 'https://notes2023.fly.dev/api/notes'
   : 'http://localhost:3001/notes'
-  
+
   return {
     entry: './src/index.js',
     output: {
