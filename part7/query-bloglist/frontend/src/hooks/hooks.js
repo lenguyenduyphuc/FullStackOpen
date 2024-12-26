e93305d17dc53bbb5c85bef85d7dcf6b0daa512d
NguyenDuyPhucLe <duyphuclenguyen@gmail.com>
2024-12-26 16:22:28 -0500

    Initialize project

--- part7/query-bloglist/frontend/src/hooks/hooks.js
+++ part7/query-bloglist/frontend/src/hooks/hooks.js
@@ -28,3 +28,5 @@ export const useTogglable = () => {
     value,
   };
 };
+
+ex
--- part8/phonebook/frontend/src/components/Authors.jsx
+++ part8/phonebook/frontend/src/components/Authors.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 const Authors = (props) => {
   if (!props.show) {
     return null;
